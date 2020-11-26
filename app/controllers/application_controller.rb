2ef499f3096f4ba19426b507dc4c04a9de5d2c06
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-11-26 16:30:11 +0200

    devise authentication from id number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,16 @@ class ApplicationController < ActionController::Base
 
   protected
 
+   def email_required?
+        false
+   end
+
   def configure_permitted_perameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:occupation, :email, :password, :name, :id_number)}
-    devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:occupation, :email, :password, :name, :id_number)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit( :email, :password, :password_confirmation, :current_password)}
+    # devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :name, :id_number)}
+    devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:identity, :name, :password, :id_number, :email) }
+
+    devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:identity, :email, :password, :name, :id_number)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit( :identity, :email, :password, :password_confirmation, :current_password, :id_number)}
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_110722) do
+ActiveRecord::Schema.define(version: 2020_11_26_135249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_110722) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "id_number"
+    t.string "identity"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
