c8d500aa7b840673e67c18bfa596affa9134aa0b
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-11-26 17:28:17 +0200

    new covid form started

--- app/controllers/covid_forms_controller.rb
+++ app/controllers/covid_forms_controller.rb
@@ -1,2 +1,34 @@
 class CovidFormsController < ApplicationController
+
+  def new
+    @user = current_user
+    @form = CovidForm.new
+  end
+
+  def create
+    @user = current_user
+    @covid_form = CovidForm.new(covid_form_params)
+    # video = Cloudinary::Uploader.upload(params[:video], :resource_type => :video)
+    # @item = covid_form.create!(video: video["url"], title: @covid_form.title, brief_id: @covid_form.brief_id)
+    # @covid_form.user_id = current_user.id
+
+    if @covid_form.save
+      redirect_to covid_form_path(@covid_form)
+      # render json: item
+    else
+      render :new
+    end
+  end
+
+def show
+  @user = current_user
+  @form = CovidForm.find(params[:id])
+
+end
+    private
+
+  def covid_form_params
+    params.require(:covid_form).permit(:user_id, :sore_throat, :shortness_of_breath, :sense_of_taste, :sense_of_taste, :cough, :awaiting_results, :past_two_weeks, :fever)
+  end
+
 end
