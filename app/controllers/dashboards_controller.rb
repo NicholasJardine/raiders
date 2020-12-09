147eb1bd2a0d953b44ac61a189fd2eaf581bb287
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-12-09 12:13:08 +0200

    dashboard in

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,6 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @covid_forms = CovidForm.where(user_id: @user.id)
-    @flagged_players = @covidforms.where(status: "Flagged")
 
   end
 end
